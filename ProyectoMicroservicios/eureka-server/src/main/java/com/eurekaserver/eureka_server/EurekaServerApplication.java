0aa535c87152fee4d4228d120bb857763fd5642a
Kevin Marulanda <77969274+KAMESX5@users.noreply.github.com>
2025-06-25 18:47:46 -0500

    Add files via upload

--- ProyectoMicroservicios/eureka-server/src/main/java/com/eurekaserver/eureka_server/EurekaServerApplication.java
+++ ProyectoMicroservicios/eureka-server/src/main/java/com/eurekaserver/eureka_server/EurekaServerApplication.java
@@ -2,14 +2,14 @@ package com.eurekaserver.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EurekaServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
 }
+
